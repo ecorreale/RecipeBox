fa3b4469e9f76ff3777b9a9ee2cf59932fc37b9f
Vannak <vannaksuos@gmail.com>
2020-06-17 21:43:43 -0400

    created search and saved components, added app.js codes

---
+++ client/src/components/Saved.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Saved() {
+  return <div>Saved</div>;
+}
+
+export default Saved;
---
+++ client/src/components/Search.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Search() {
+  return (
+    <div className="input-group mb-3" style={{ width: '30%' }}>
+      <div className="input-group-prepend">
+        <button
+          className="btn btn-outline-secondary"
+          type="button"
+          id="button-addon1"
+        >
+          Search
+        </button>
+      </div>
+      <input
+        type="text"
+        className="form-control"
+        placeholder=""
+        aria-label="Example text with button addon"
+        aria-describedby="button-addon1"
+      />
+    </div>
+  );
+}
+
+export default Search;
