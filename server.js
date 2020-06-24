8c49e16a66e4389d3bc6cbf9b5ad5bffaf4d2425
Vannak <vannaksuos@gmail.com>
2020-06-23 21:43:28 -0400

    working on api in server.js

--- server.js
+++ server.js
@@ -17,6 +17,52 @@ app.use(cookieParser());
 
 // Include any API routes here
 
+//Van working on api
+
+// app.get("/api/edamam", (req, res) => {
+//   recipeModel.find().then(savedRecipes => {
+//       res.json(savedRecipes)
+//   })
+// });
+
+// app.post("/api/books", ({ body }, res) => {
+//   // const recipe = body;
+//   // console.log(recipe)
+//   console.log(RecipeModel)
+//   const recipe = {
+//       title:body.bookInfo.volumeInfo.title,
+//       authors:body.bookInfo.volumeInfo.authors,
+//       description:body.bookInfo.volumeInfo.description,
+//       image:body.bookInfo.volumeInfo.imageLinks && body.bookInfo.volumeInfo.imageLinks[0],
+//       links:body.bookInfo.volumeInfo.previewLInk
+//   }
+//   recipeModel.create(recipe, (error, savedRecipes) => {
+//   if (error) {
+//       console.log(error);
+//   } else {
+//       res.send(savedRecipes);
+//   }
+//   });
+// });
+
+// app.delete("/api/edamam/:id",  (req, res) => {
+//   recipeModel.remove({_id:req.params.id}, (error, response) => {
+//       if (error) {
+//           res.send(error);
+//       } else {
+//           res.send(response);
+//       }
+//       });
+//   });
+
+// app.get("/api/edamam", (req, res) => {
+//   console.log(req.query)
+//   axios.get("https://api.edamam.com/search?q=" + req.query.q).then((response) => {
+//   // console.log(response.data)
+//   res.json(response.data)
+//   })
+// })
+
 // Serve static files from the React app in production.
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client', 'build')));
