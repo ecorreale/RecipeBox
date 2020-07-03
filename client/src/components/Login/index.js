6a9c5f8bdb83344010f070863e87a2f6c8afb99a
Ernest Correale <ecorreale@me.com>
2020-07-02 23:30:26 -0400

    Commented out isEmail validator until ready

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -3,7 +3,7 @@ import Form from 'react-validation/build/form';
 import Input from 'react-validation/build/input';
 import CheckButton from 'react-validation/build/button';
 
-import { isEmail } from 'validator';
+// import { isEmail } from 'validator';
 import AuthService from '../../services/auth.service';
 
 // Required Field Alerter
