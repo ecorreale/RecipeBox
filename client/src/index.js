d2a782cee2e2584651dcbc2646a4f42e0d77d967
Ernest Correale <ecorreale@me.com>
2020-07-02 22:32:30 -0400

    Added browserRouter wrapper

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  // <React.StrictMode>
-  <App />,
-  // </React.StrictMode>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+
   document.getElementById('root')
 );
 
